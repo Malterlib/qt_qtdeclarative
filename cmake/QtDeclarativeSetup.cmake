8531de279823e36807c2a10c627678742a0aad86
Erik Olofsson <erik@olofsson.info>
2023-05-30 16:00:56 +0200

    Assume python3 is available

--- cmake/QtDeclarativeSetup.cmake
+++ cmake/QtDeclarativeSetup.cmake
@@ -18,8 +18,6 @@ function(qt_declarative_write_tag_header target_name)
     configure_file("qml_compile_hash_p.h.in" "${CMAKE_CURRENT_BINARY_DIR}/qml_compile_hash_p.h")
 endfunction()
 
-find_package(PythonInterp REQUIRED)
-
 # Generate a header file containing a regular expression jit table.
 function(qt_declarative_generate_reg_exp_jit_tables consuming_target)
     set(generate_dir "${CMAKE_CURRENT_BINARY_DIR}/.generated")
@@ -34,7 +32,7 @@ function(qt_declarative_generate_reg_exp_jit_tables consuming_target)
 
     add_custom_command(
         OUTPUT "${output_file}" "/DIR:${generate_dir}"
-        COMMAND "${PYTHON_EXECUTABLE}" ${retgen_script_file} ${output_file}
+        COMMAND "python3" ${retgen_script_file} ${output_file}
         MAIN_DEPENDENCY ${retgen_script_file}
     )
     target_sources(${consuming_target} PRIVATE ${output_file})
@@ -43,7 +41,7 @@ endfunction()
 
 function(qt_qml_find_python out_var_path out_var_found)
     find_program(QT_QML_PYTHON_PATH
-                 NAMES python python2 python3 py
+                 NAMES python3 python2 python py
                  DOC "Qt Declarative python path")
     if(QT_QML_PYTHON_PATH)
         set(${out_var_path} "${QT_QML_PYTHON_PATH}" PARENT_SCOPE)
